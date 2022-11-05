0e8a9db4b6883f20ef68813c4e91b0cf528dc077
Adam Sranko <srankoadam@gmail.com>
2022-11-05 19:22:41 +0100

    Fixed modularisStart bash

--- modularisStart.sh
+++ modularisStart.sh
@@ -3,14 +3,23 @@
 echo "Starting modularis main"
 dotnet /app/ModularisWebInterface/ModularisWebInterface.dll > /dev/null &
 
+echo "Waiting 20 secfor startup"
 sleep 20
+
+echo "Healthy result:"
 healthyResult=$(curl http://localhost:5000/healthy)
 echo $healthyResult
+
+echo "Setting workdir /app/ProjectModularisBot"
+cd /app/ProjectModularisBot
 while [[ $healthyResult != *"Healthy"* ]]; 
 do
-    cd /app/ProjectModularisBot
-    dotnet /app/ProjectModularisBot/ProjectModularisBot.dll > /dev/null &
+    echo "Healthy result RECHECK:"
     healthyResult=$(curl http://localhost:5000/healthy)
     echo $healthyResult
+    echo "Waiting 5 sec before retrying"
     sleep 5
 done
+echo "All ok starting bot"
+dotnet /app/ProjectModularisBot/ProjectModularisBot.dll > /dev/null &
+    
