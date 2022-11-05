9f572cbdf935d8e2a0e94ac068fd4db62dfda9b5
Adam Sranko <srankoadam@gmail.com>
2022-11-05 14:39:12 +0100

    set path for bot

--- modularisStart.sh
+++ modularisStart.sh
@@ -7,6 +7,7 @@ healthyResult=$(curl http://localhost:5000/healthy)
 echo $healthyResult
 while [[ $healthyResult != *"Healthy"* ]]; 
 do
+    cd /app/ProjectModularisBot
     dotnet /app/ProjectModularisBot/ProjectModularisBot.dll &
     healthyResult=$(curl http://localhost:5000/healthy)
     echo $healthyResult
