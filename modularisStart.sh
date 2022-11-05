2f028443e15ec9f68958025dae47b059f8eaec78
Adam Sranko <srankoadam@gmail.com>
2022-11-05 19:00:47 +0100

    take2 startsh

--- modularisStart.sh
+++ modularisStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting modularis main"
-dotnet /app/ModularisWebInterface/ModularisWebInterface.dll &
+dotnet /app/ModularisWebInterface/ModularisWebInterface.dll > /dev/null &
 
 sleep 20
 healthyResult=$(curl http://localhost:5000/healthy)
@@ -9,7 +9,7 @@ echo $healthyResult
 while [[ $healthyResult != *"Healthy"* ]]; 
 do
     cd /app/ProjectModularisBot
-    dotnet /app/ProjectModularisBot/ProjectModularisBot.dll &
+    dotnet /app/ProjectModularisBot/ProjectModularisBot.dll > /dev/null &
     healthyResult=$(curl http://localhost:5000/healthy)
     echo $healthyResult
     sleep 5
