1bafc8243e0a517fb75534d4a1fa6c357e429afa
Adam Sranko <srankoadam@gmail.com>
2022-11-05 14:05:08 +0100

    start script added bot start as well

--- modularisStart.sh
+++ modularisStart.sh
@@ -3,3 +3,11 @@
 echo "Starting modularis main"
 dotnet /app/ModularisWebInterface/ModularisWebInterface.dll
 
+healthyResult=$(curl http://localhost:5000/healthy)
+echo $healthyResult
+while [[ $healthyResult != *"Healthy"* ]]; 
+do
+    dotnet /app/ProjectModularisBot/ProjectModularisBot.dll
+    healthyResult=$(curl http://localhost:5000/healthy)
+    echo $healthyResult
+done
