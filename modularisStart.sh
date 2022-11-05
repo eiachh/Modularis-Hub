d435d77af7c9d38fed63fc2716f708071e31e9c6
Adam Sranko <srankoadam@gmail.com>
2022-11-05 14:32:55 +0100

    run app in detached mode

--- modularisStart.sh
+++ modularisStart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Starting modularis main"
-dotnet /app/ModularisWebInterface/ModularisWebInterface.dll
+dotnet /app/ModularisWebInterface/ModularisWebInterface.dll &
 
 healthyResult=$(curl http://localhost:5000/healthy)
 echo $healthyResult
 while [[ $healthyResult != *"Healthy"* ]]; 
 do
-    dotnet /app/ProjectModularisBot/ProjectModularisBot.dll
+    dotnet /app/ProjectModularisBot/ProjectModularisBot.dll &
     healthyResult=$(curl http://localhost:5000/healthy)
     echo $healthyResult
 done
